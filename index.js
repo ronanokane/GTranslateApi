287c7d8fb9080a06d40e45afdede18a18dc68a09
ronanokane <ronan.okane@gmail.com>
2023-01-18 18:56:54 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const translate = require('@iamtraction/google-translate');
+const express = require('express');
+const app = express();
+
+app.get('/:langCode/:text', async (req, res)=>{
+    translate(req.params.text, {to: req.params.langCode})
+    .then(({text})=>{
+        res.send({text: text});
+    })
+    .catch((err)=>{
+        res.status(404).send({error: true,message: err.message});
+    })
+    .catch((err)=>{
+        console.log(err.message);
+    });
+});
+
+const port=process.argv[2];
+app.listen(port, ()=>console.log('listening on port ' + port));
